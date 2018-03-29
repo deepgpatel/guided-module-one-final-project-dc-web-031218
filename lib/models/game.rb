f85c8ac63315a81b324a35b282293d65cdd05d30
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-03-29 16:27:07 -0400

    more updates

--- lib/models/game.rb
+++ lib/models/game.rb
@@ -10,7 +10,7 @@ class Game < ActiveRecord::Base
 
   def user_employment
     # percentage of users that own this game that are employed
-    output_array = {}
+    output_hash = {}
     instances_array = collect_all_instances_with_this_title
     #use private method, to add in for all consoles
     instances_array.each do |game_on_console|
@@ -62,6 +62,7 @@ class Game < ActiveRecord::Base
   end
 
   def game_on_console_not_nil_1_user_avg_age(game_on_console, hash)
+    # get averages, push to hash
     users = game_on_console.users
     sum_age = 0
     users.each {|user| sum_age += user.age}
@@ -74,6 +75,22 @@ class Game < ActiveRecord::Base
   end
 
   def game_on_console_not_nil_2_user_employment(game_on_console, hash)
+    # get employment percentages, push to hash
     users = game_on_console.users
+
+    employed_count = 0
+    users.each do |user|
+      if user.employed
+        employed_count += 1
+      end
+    end
+    if users.size == 0
+      hash[game_on_console.console] = nil
+    else
+      employment_ratio = employed_count.to_f / users.size
+      employment_percentage = employment_ratio * 100
+      hash[game_on_console.console] = "#{employment_percentage} %"
+    end
+
   end
 end
